6fcb1e747970000498cfed1dd8cf35083621319a
junseokyun <roadvirushn@gmail.com>
2025-10-29 15:52:18 +0000

    [front] Adjust css of footer

--- src/front/components/Footer/DeckInput/DeckInput.tsx
+++ src/front/components/Footer/DeckInput/DeckInput.tsx
@@ -3,9 +3,9 @@ import DecksSvg from "@/public/Decks-Svg.svg";
 const DeckInput = ({}) => {
   const {decks} = useAnkiConnectionStore();
   return (
-    <div style={{display: 'flex', alignItems: 'center', gap: '5px', height: '100%'}}>
+    <div style={{display: 'flex', alignItems: 'center', height: '100%'}}>
       <DecksSvg width={18} height={18} style={{ display: 'block', flexShrink: 0 }}/>
-      <select id="deck-select" name="deck-select" style={{height: '20px', width: '180px', padding: '0 4px'}}>
+      <select id="deck-select" name="deck-select" style={{height: '20px', width: '180px'}}>
         {decks.length>0? decks.map((deck) => <option key={deck} value={deck}>{deck}</option>) : <option>Check Anki Connection!</option>}
       </select>
     </div>
--- src/front/components/Footer/ManualCardAddBtn/ManualCardAddBtn.tsx
+++ src/front/components/Footer/ManualCardAddBtn/ManualCardAddBtn.tsx
@@ -1,4 +1,9 @@
+import AddAnkiBtnSvg from '@/public/AddAnkiBtn-Svg.svg';
 const ManualCardAddBtn = ({}) => {
-  return <div></div>;
+  return (<div style={{position: 'relative'}}>
+    <button style={{ all: 'unset', cursor: 'pointer' }} title="Add Card to Anki Manually" onClick={() => {}}>
+      <AddAnkiBtnSvg width={40} height={40} style={{ position: 'fixed',bottom: '1px', right: '5px', filter: 'drop-shadow(3px 3px 2px #333)' }}/>
+    </button>
+  </div>);
 };
 export default ManualCardAddBtn;
