77da26b5e4506e074364620ab28b17c6d09dce04
junseokyun <roadvirushn@gmail.com>
2026-02-09 15:13:31 +0000

    [front] add react-base content elements files

---
+++ src/content/Menu.tsx
@@ -0,0 +1,8 @@
+import menuStyles from "./Menu.module.css";
+
+const Menu = ({}) => {
+  return <div className={menuStyles["extension-overlay"]}>
+
+  </div>;
+};
+export default Menu;
---
+++ src/content/Tooltip.tsx
@@ -0,0 +1,7 @@
+import tooltipStyles from "./Tooltip.module.css";
+const Tooltip = ({}) => {
+  return <div className={tooltipStyles["extension-tooltip"]}>
+
+  </div>;
+};
+export default Tooltip;
