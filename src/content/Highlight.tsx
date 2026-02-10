2e8009e13c5a432651630e7885a0a2860aeecf61
junseokyun <roadvirushn@gmail.com>
2026-02-10 14:33:26 +0000

    [front]add react content component

---
+++ src/content/Highlight.tsx
@@ -0,0 +1,8 @@
+import highlightStyles from "./Highlight.module.css";
+
+const Highlight = ({}) => {
+  return <div className={highlightStyles.highlight}>
+
+  </div>;
+};
+export default Highlight;
--- src/content/Menu.tsx
+++ src/content/Menu.tsx
@@ -1,8 +1,8 @@
 import menuStyles from "./Menu.module.css";
 
 const Menu = ({}) => {
-  return <div className={menuStyles["extension-overlay"]}>
-
+  return <div className={menuStyles.menu}>
+    <div className={menuStyles.header}></div>
   </div>;
 };
 export default Menu;
