a096665c31d5134005c2398999c9150cbc2a543c
junseokyun <roadvirushn@gmail.com>
2026-03-11 15:57:19 +0000

    [front] set init teampltate data

--- src/scripts/background/constants.ts
+++ src/scripts/background/constants.ts
@@ -1,1 +1,25 @@
+import { Template, TemplateFieldDataType } from '@/front/utils/useTemplates';
+
 export const STORAGE_KEY = 'anki-card-wizard-global-var-store';
+export const defaultTemplates: Template[] = [
+  {
+    meta: {},
+    templateName: 'Default Template',
+    modelName: 'Basic',
+    urlPatterns: [],
+    rootTag: 'body',
+    Front: {
+      html: '{{Front}}',
+      fields: [
+        { name: 'Front', content: '', dataType: TemplateFieldDataType.TEXT, isOptional: false },
+      ],
+    },
+    Back: {
+      html: '{{Back}}',
+      fields: [
+        { name: 'Back', content: '', dataType: TemplateFieldDataType.TEXT, isOptional: false },
+      ],
+    },
+    tags: [],
+  },
+];
--- src/scripts/background/functions.ts
+++ src/scripts/background/functions.ts
@@ -1,13 +1,24 @@
-import useConfigure, { Language } from '@/front/utils/useConfigure';
+import useConfigure, { Language, ThemeSetting } from '@/front/utils/useConfigure';
+import useTemplate from '@/front/utils/useTemplates';
+import { defaultTemplates } from './constants';
 
 export const onInstalled = () => {
-  // TODO: setting default template.
-  const uiLanguage = chrome.i18n.getUILanguage();
-  const defaultLang = uiLanguage.startsWith('ko') ? Language.KO : Language.EN;
-  console.log('Detected UI Language:', uiLanguage, 'Setting default language to:', defaultLang);
-  useConfigure.getState().setLanguage(defaultLang);
-  console.log('Extension installed or updated. Default language set to:', useConfigure.getState().language);
-
+  if (!useConfigure.getState().language) {
+    const uiLanguage = chrome.i18n.getUILanguage();
+    const defaultLang = uiLanguage.startsWith('ko') ? Language.KO : Language.EN;
+    console.log('Detected UI Language:', uiLanguage, 'Setting default language to:', defaultLang);
+    useConfigure.getState().setLanguage(defaultLang);
+    console.log('Extension installed or updated. Default language set to:', useConfigure.getState().language);
+  }
+  if (!useConfigure.getState().themeOption) {
+    useConfigure.getState().setThemeSetting(ThemeSetting.NONE);
+  }
+  if (!useConfigure.getState().fontSize) {
+    useConfigure.getState().setFontSize('normal');
+  }
+  for (const template of defaultTemplates) {
+    useTemplate.getState().addTemplate(template);
+  }
 };
 export const getCurrentTabId = async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
