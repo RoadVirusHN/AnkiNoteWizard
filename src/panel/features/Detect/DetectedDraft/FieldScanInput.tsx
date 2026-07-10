f8122489aebff4b2d62586caab200b047d5052db
junseokyun <roadvirushn@gmail.com>
2026-07-10 14:54:25 +0000

    [chore] first testing quill.js

--- src/panel/features/Detect/DetectedDraft/FieldScanInput.tsx
+++ src/panel/features/Detect/DetectedDraft/FieldScanInput.tsx
@@ -2,7 +2,8 @@ import { FieldData } from "@/types/scanRule.types";
 import detectPageStyle from "../detectPage.module.css";
 import { useTranslation } from "react-i18next";
 import { onFieldDrop, onFieldPaste } from "@/panel/utils/functions";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useRef } from "react";
+import Quill from "quill";
 
 const MAX_CONTENT_LENGTH = 100;
 const FieldScanInput = ({field, isEditing, setCurrentField}:{field:FieldData, isEditing: boolean, setCurrentField:(newField:FieldData)=>void}) => {
@@ -10,8 +11,24 @@ const FieldScanInput = ({field, isEditing, setCurrentField}:{field:FieldData, is
   const containedTooManyEmpty = field.content.length - renderedContent.length > 30;
   const {t} = useTranslation('components', {keyPrefix:'fieldScanInput'});
   const onChange = (e:ChangeEvent<HTMLTextAreaElement>)=>{setCurrentField({key: field.key, content: e.target.value ?? ''})};
-  return <div className={detectPageStyle.fieldInput}>
-    <label 
+  const editorRef = useRef<HTMLDivElement>(null);
+  useEffect(()=>{
+    if (editorRef.current) {
+      const quill = new Quill(editorRef.current,
+        {
+          debug: 'warn',
+          theme: 'snow',
+          modules: {
+            toolbar: true
+          },
+          readOnly: !isEditing,
+          placeholder: field.content
+        }
+      );
+    }
+  },[isEditing]);
+  return <div className={detectPageStyle.fieldInput} ref={editorRef}>
+    {/* <label 
     className={`${detectPageStyle.fieldLabel}` + (containedTooManyEmpty ? ` ${detectPageStyle.veryEmpty}` : '')}
     htmlFor="content"
     title={containedTooManyEmpty ?t('containedTooManyEmptyWarn'):''}
@@ -31,7 +48,7 @@ const FieldScanInput = ({field, isEditing, setCurrentField}:{field:FieldData, is
         onDragOver={(e)=>{e.preventDefault()}}
       /> :
       <div>{field.content.length > MAX_CONTENT_LENGTH ? field.content.slice(0,MAX_CONTENT_LENGTH)+'...' : field.content}</div>
-    }
+    } */}
   </div>;
 };
 export default FieldScanInput;
