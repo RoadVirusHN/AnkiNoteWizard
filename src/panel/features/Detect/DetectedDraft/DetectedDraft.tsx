187255583bc38e09ab260f2a2e837adffc2fdf4b
junseokyun <roadvirushn@gmail.com>
2026-07-11 15:01:13 +0000

    [front] fix detectedDraft DOM structure && apply testing quill.js

--- src/panel/features/Detect/DetectedDraft/DetectedDraft.tsx
+++ src/panel/features/Detect/DetectedDraft/DetectedDraft.tsx
@@ -1,5 +1,5 @@
 import useScanRule from "@/panel/stores/useScanRule";
-import detectPageStyle from "@/panel/features/Detect/detectPage.module.css";
+import detectedDraftStyles from "@/panel/features/Detect/DetectedDraft/detectedDraft.module.css";
 import useGlobalVarStore from "@/panel/stores/useGlobalVarStore";
 import { Draft, FieldData, ScanRule } from "@/types/scanRule.types";
 import FieldScanInput from "./FieldScanInput";
@@ -50,26 +50,56 @@ const DetectedDraft = ({idx, note, scanRuleId, checkAdd}:DetectedDraftProps) => 
     setCurrentDraft(note);
   }, [note]);
   return (  
-  <article className={`${detectPageStyle.detectedDraftContainer}` + (isEditing? ` ${detectPageStyle.editing}` : '')} 
+  <article className={`${detectedDraftStyles.detectedDraftContainer}` + (isEditing? ` ${detectedDraftStyles.editing}` : '')} 
   onClick={onClick} title={isEditing ? tDraft('clickToStopEditingAndRevert') : tDraft('clickToEdit')}>
-    {
-      !isEditing ?<input type="checkbox" onChange={e=>{checkAdd(e.target.checked)}} onClick={e=>e.stopPropagation()}/> : null
-    }
-    <div className={detectPageStyle.detectedDraftContent}>
-      <div className={detectPageStyle.detectedDraftHeader}>
-        <span className={detectPageStyle.scanRuleName} title={t('scanRule')} >{scanRuleId}</span>
-        <div className={detectPageStyle.tagTrap}>
-          <Tags 
-            isModifying={isEditing} 
-            givenTagIds={currentDraft.tagIds}
-            onAddTag={(newTag)=>{
-              setCurrentDraft({...currentDraft,tagIds:[...currentDraft.tagIds, newTag.name]});
-              setIsChanged(true);
-            }}
-            onRemoveTag={(targetTag)=>{
-              setCurrentDraft({...currentDraft,tagIds:[...currentDraft.tagIds].filter((tagName)=>tagName!==targetTag.name)});
-              setIsChanged(true);
-          }}/>
+    <div className={detectedDraftStyles.detectedDraftContent}>
+      <div className={detectedDraftStyles.detectedDraftHeader}>
+        <div className={detectedDraftStyles.scanRuleNameContainer}>
+          {
+            !isEditing &&<input type="checkbox" className={detectedDraftStyles.checkBox} onChange={e=>{checkAdd(e.target.checked)}} onClick={e=>e.stopPropagation()}/>
+          }
+          <span className={detectedDraftStyles.scanRuleName} title={t('scanRule')} >{scanRuleId}</span>
+          <div className={detectedDraftStyles.tagTrap} style={{pointerEvents: isEditing ? 'auto' : 'none'}}>
+            <Tags 
+              isModifying={isEditing} 
+              givenTagIds={currentDraft.tagIds}
+              onAddTag={(newTag)=>{
+                setCurrentDraft({...currentDraft,tagIds:[...currentDraft.tagIds, newTag.name]});
+                setIsChanged(true);
+              }}
+              onRemoveTag={(targetTag)=>{
+                setCurrentDraft({...currentDraft,tagIds:[...currentDraft.tagIds].filter((tagName)=>tagName!==targetTag.name)});
+                setIsChanged(true);
+              }}/>
+          </div>
+        </div>
+        <div className={detectedDraftStyles.buttons}>
+        {
+          isEditing ? 
+          <>
+            <img title={tDraft('extractData')} src={MagicIcon} onClick={(e)=>{
+              e.stopPropagation();
+              enterInspectionMode();
+            }}/> 
+            <img src={SaveIcon} onClick={(e)=>{
+              e.stopPropagation();
+              updateDraft(idx,
+                {
+                  fields: currentDraft.fields,
+                  tagIds: currentDraft.tagIds
+                }
+              );
+              setIsEditing(false);
+            }}/>
+            {isChanged &&
+              <img src={ResetIcon} onClick={onReset}/>}
+            </> :
+          <img src={DelIcon} onClick={(e)=>{
+            e.stopPropagation();
+            removeDraft(idx);
+            setCurrentDetected(currentDetected - 1);
+          }} style={{cursor: 'pointer'}}/>
+        }
         </div>
       </div>
       {
@@ -82,34 +112,6 @@ const DetectedDraft = ({idx, note, scanRuleId, checkAdd}:DetectedDraftProps) => 
           }}/>
         })
       }
-    </div>
-    <div className={detectPageStyle.button}>
-    {
-      isEditing ? 
-      <>
-        <img title={tDraft('extractData')} src={MagicIcon} onClick={(e)=>{
-          e.stopPropagation();
-          enterInspectionMode();
-        }}/> 
-        <img src={SaveIcon} onClick={(e)=>{
-          e.stopPropagation();
-          updateDraft(idx,
-            {
-              fields: currentDraft.fields,
-              tagIds: currentDraft.tagIds
-            }
-          );
-          setIsEditing(false);
-        }}/>
-        {isChanged &&
-          <img src={ResetIcon} onClick={onReset}/>}
-        </> :
-      <img src={DelIcon} onClick={(e)=>{
-        e.stopPropagation();
-        removeDraft(idx);
-        setCurrentDetected(currentDetected - 1);
-      }} style={{cursor: 'pointer'}}/>
-    }
     </div>
   </article>);
 };
--- src/panel/features/Detect/DetectedDraft/FieldScanInput.tsx
+++ src/panel/features/Detect/DetectedDraft/FieldScanInput.tsx
@@ -1,54 +1,67 @@
 import { FieldData } from "@/types/scanRule.types";
-import detectPageStyle from "../detectPage.module.css";
+import detectedDraftStyles from "@/panel/features/Detect/DetectedDraft/detectedDraft.module.css";
 import { useTranslation } from "react-i18next";
 import { onFieldDrop, onFieldPaste } from "@/panel/utils/functions";
 import { ChangeEvent, useEffect, useRef } from "react";
 import Quill from "quill";
+import 'quill/dist/quill.snow.css';
+import { editor } from "monaco-editor";
 
 const MAX_CONTENT_LENGTH = 100;
 const FieldScanInput = ({field, isEditing, setCurrentField}:{field:FieldData, isEditing: boolean, setCurrentField:(newField:FieldData)=>void}) => {
   const renderedContent = field.content.replace(/\s+/g, ' ').trim();
   const containedTooManyEmpty = field.content.length - renderedContent.length > 30;
   const {t} = useTranslation('components', {keyPrefix:'fieldScanInput'});
-  const onChange = (e:ChangeEvent<HTMLTextAreaElement>)=>{setCurrentField({key: field.key, content: e.target.value ?? ''})};
   const editorRef = useRef<HTMLDivElement>(null);
+  const isMounted = useRef(false);
   useEffect(()=>{
-    if (editorRef.current) {
-      const quill = new Quill(editorRef.current,
-        {
-          debug: 'warn',
-          theme: 'snow',
-          modules: {
-            toolbar: true
-          },
-          readOnly: !isEditing,
-          placeholder: field.content
-        }
-      );
+    if (!editorRef.current) return;
+    if (!isMounted.current) {
+      // prevent double toolbar by strict mode
+      isMounted.current = true;
+      return;
     }
-  },[isEditing]);
-  return <div className={detectPageStyle.fieldInput} ref={editorRef}>
-    {/* <label 
-    className={`${detectPageStyle.fieldLabel}` + (containedTooManyEmpty ? ` ${detectPageStyle.veryEmpty}` : '')}
-    htmlFor="content"
-    title={containedTooManyEmpty ?t('containedTooManyEmptyWarn'):''}
-    >{field.key}</label>
-    {
-      isEditing ? 
-      <textarea
-        id='content'
-        className={detectPageStyle.field}
-        value={field.content}
-        onClick={(e)=>{
-          e.stopPropagation();
-        }}
-        onChange={onChange}
-        onPaste={onFieldPaste(onChange)}
-        onDrop={onFieldDrop(onChange)}
-        onDragOver={(e)=>{e.preventDefault()}}
-      /> :
-      <div>{field.content.length > MAX_CONTENT_LENGTH ? field.content.slice(0,MAX_CONTENT_LENGTH)+'...' : field.content}</div>
-    } */}
-  </div>;
+    const quill = new Quill(editorRef.current,
+      {
+        debug: 'warn',
+        theme: 'snow',
+        modules: {
+          toolbar: true
+        }
+      }
+    );
+    quill.root.innerHTML = field.content;
+    quill.on('text-change', function(delta, oldDelta, source) {
+      if (source === 'user') {
+        const newContent = quill.root.innerHTML;
+        setCurrentField({key: field.key, content: newContent});
+      }
+    });
+    const parent = editorRef.current.parentElement;
+    console.log(parent);
+    return ()=>{
+      quill.off('text-change');
+    };
+  },[]);
+  // onChange={onChange}
+  // onPaste={onFieldPaste(onChange)}
+  // onDrop={onFieldDrop(onChange)}
+  // onDragOver={(e)=>{e.preventDefault()}}
+  return <div className={detectedDraftStyles.fieldInput}>
+       <label 
+      className={`${detectedDraftStyles.fieldLabel}` + (containedTooManyEmpty ? ` ${detectedDraftStyles.veryEmpty}` : '')}
+      htmlFor="content"
+      title={containedTooManyEmpty ?t('containedTooManyEmptyWarn'):''}
+      >{field.key}</label>
+      <div className={detectedDraftStyles.field} style={{display: isEditing ? 'block' : 'none'}} onClick={(e)=>{e.stopPropagation();}} >
+        <div
+          id='content'
+          ref={editorRef}
+          /> 
+      </div>
+      <div
+      style={{display: isEditing ? 'none' : 'block'}}
+      >{field.content.length > MAX_CONTENT_LENGTH ? field.content.slice(0,MAX_CONTENT_LENGTH)+'...' : field.content}</div>
+    </div>;
 };
 export default FieldScanInput;
